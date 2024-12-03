a392ffc6f4f4b3df6daec8318361e7c8a706a0b9
Nadahar <Nadahar@users.noreply.github.com>
2024-12-03 20:52:10 +0100

    JavaDocs

--- bundles/org.openhab.binding.milllan/src/main/java/org/openhab/binding/milllan/internal/api/response/ControlStatusResponse.java
+++ bundles/org.openhab.binding.milllan/src/main/java/org/openhab/binding/milllan/internal/api/response/ControlStatusResponse.java
@@ -31,126 +31,147 @@ import com.google.gson.annotations.SerializedName;
  * @author Nadahar - Initial contribution
  */
 @NonNullByDefault
-public class ControlStatusResponse implements Response { // TODO: (Nad) JavaDocs
+public class ControlStatusResponse implements Response {
 
-    /**
-     * The temperature measured by sensor in Celsius degrees (with calibration offset value included).
-     */
+    /** The temperature measured by the sensor in °C (with calibration offset value included) */
     @SerializedName("ambient_temperature")
     @Nullable
     private Double ambientTemperature;
 
-    /**
-     * The current heating power in Watts.
-     */
+    /** The current heating power in W */
     @SerializedName("current_power")
     @Nullable
     private Double currentPower;
 
-    /**
-     * The control signal of the PID regulator (0-100%).
-     */
+    /** The control signal of the PID regulator (0-100%) */
     @SerializedName("control_signal")
     @Nullable
     private Double controlSignal;
 
-    /**
-     * The lock status.
-     */
+    /** The lock status */
     @SerializedName("lock_active")
     @Nullable
-    private LockStatus lockActive;
+    private LockStatus lockStatus;
 
-    /**
-     * The open widows status.
-     */
+    /** The open widows status */
     @SerializedName("open_window_active_now")
     @Nullable
     private OpenWindowStatus openWindowStatus;
 
-    /**
-     * The temperature measured by sensor in Celsius degrees without calibration offset value.
-     */
+    /** The temperature measured by the sensor °C degrees without the calibration offset value */
     @SerializedName("raw_ambient_temperature")
     @Nullable
     private Double rawAmbientTemperature;
 
-    /**
-     * The current set temperature in Celsius degrees.
-     */
+    /** The current set temperature in °C */
     @SerializedName("set_temperature")
     @Nullable
     private Double setTemperature;
 
     /**
-     * {@code true} if the device is switched on - whether it is set to working, with heating.
+     * The API documentation claims that this is supposed to be: {@code true} if the device
+     * is switched on - whether it is set to working, with heating.
+     * <p>
+     * In practice however, it always seems to return {@code false}.
      */
     @SerializedName("switched_on")
     @Nullable
     private Boolean switchedOn;
 
-    /**
-     * Whether the device has connection with the cloud.
-     */
+    /** Whether the device is allowed to connect with the cloud */
     @SerializedName("connected_to_cloud")
     @Nullable
     private Boolean connectedToCloud;
 
-    /**
-     * The current mode of operation.
-     */
+    /** The current mode of operation */
     @SerializedName("operation_mode")
     @Nullable
     private OperationMode operatingMode;
 
+    /** The device API's {@code HTTP Response Status} */
     @Nullable
     private ResponseStatus status;
 
+    /**
+     * @return The temperature measured by the sensor in °C (with calibration offset value included).
+     */
     @Nullable
     public Double getAmbientTemperature() {
         return ambientTemperature;
     }
 
+    /**
+     * @return The current heating power in W.
+     */
     @Nullable
     public Double getCurrentPower() {
         return currentPower;
     }
 
+    /**
+     * @return The control signal of the PID regulator (0-100%).
+     */
     @Nullable
     public Double getControlSignal() {
         return controlSignal;
     }
 
+    /**
+     * @return The {@link LockStatus}.
+     */
     @Nullable
-    public LockStatus getLockActive() {
-        return lockActive;
+    public LockStatus getLockStatus() {
+        return lockStatus;
     }
 
+    /**
+     * @return The {@link OpenWindowStatus}.
+     */
     @Nullable
     public OpenWindowStatus getOpenWindowStatus() {
         return openWindowStatus;
     }
 
+    /**
+     * @return The temperature measured by the sensor °C degrees without the calibration offset value.
+     */
     @Nullable
     public Double getRawAmbientTemperature() {
         return rawAmbientTemperature;
     }
 
+    /**
+     * @return The current set temperature in °C.
+     */
     @Nullable
     public Double getSetTemperature() {
         return setTemperature;
     }
 
+    /**
+     * The API documentation claims that this is supposed to be: {@code true} if the device
+     * is switched on - whether it is set to working, with heating.
+     * <p>
+     * In practice however, it always seems to return {@code false}.
+     *
+     * @return {@code false}.
+     */
     @Nullable
     public Boolean getSwitchedOn() {
         return switchedOn;
     }
 
+    /**
+     * @return {@code true} if the device is allowed to connect with the cloud.
+     */
     @Nullable
     public Boolean getConnectedToCloud() {
         return connectedToCloud;
     }
 
+    /**
+     * @return The {@link OperationMode}.
+     */
     @Nullable
     public OperationMode getOperatingMode() {
         return operatingMode;
@@ -164,7 +185,7 @@ public class ControlStatusResponse implements Response { // TODO: (Nad) JavaDocs
 
     @Override
     public int hashCode() {
-        return Objects.hash(ambientTemperature, connectedToCloud, controlSignal, currentPower, lockActive,
+        return Objects.hash(ambientTemperature, connectedToCloud, controlSignal, currentPower, lockStatus,
                 openWindowStatus, operatingMode, rawAmbientTemperature, setTemperature, status, switchedOn);
     }
 
@@ -180,14 +201,14 @@ public class ControlStatusResponse implements Response { // TODO: (Nad) JavaDocs
             return false;
         }
         ControlStatusResponse other = (ControlStatusResponse) obj;
-        return Objects.equals(ambientTemperature, other.ambientTemperature)
-                && Objects.equals(connectedToCloud, other.connectedToCloud)
-                && Objects.equals(controlSignal, other.controlSignal)
-                && Objects.equals(currentPower, other.currentPower) && lockActive == other.lockActive
-                && openWindowStatus == other.openWindowStatus && operatingMode == other.operatingMode
-                && Objects.equals(rawAmbientTemperature, other.rawAmbientTemperature)
-                && Objects.equals(setTemperature, other.setTemperature) && status == other.status
-                && Objects.equals(switchedOn, other.switchedOn);
+        return Objects.equals(ambientTemperature, other.ambientTemperature) &&
+            Objects.equals(connectedToCloud, other.connectedToCloud) &&
+            Objects.equals(controlSignal, other.controlSignal) &&
+            Objects.equals(currentPower, other.currentPower) && lockStatus == other.lockStatus &&
+            openWindowStatus == other.openWindowStatus && operatingMode == other.operatingMode &&
+            Objects.equals(rawAmbientTemperature, other.rawAmbientTemperature) &&
+            Objects.equals(setTemperature, other.setTemperature) && status == other.status &&
+            Objects.equals(switchedOn, other.switchedOn);
     }
 
     @Override
@@ -203,8 +224,8 @@ public class ControlStatusResponse implements Response { // TODO: (Nad) JavaDocs
         if (controlSignal != null) {
             builder.append("controlSignal=").append(controlSignal).append(", ");
         }
-        if (lockActive != null) {
-            builder.append("lockActive=").append(lockActive).append(", ");
+        if (lockStatus != null) {
+            builder.append("lockActive=").append(lockStatus).append(", ");
         }
         if (openWindowStatus != null) {
             builder.append("openWindowStatus=").append(openWindowStatus).append(", ");
